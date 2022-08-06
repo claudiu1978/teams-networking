34ed708b986b5558f661a8c5e741156be9d0e467
Claudiu Cimpean <cimpean1@gmail.com>
2022-08-06 16:45:32 +0300

    extract getTeamHTML functiuo

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
+function getTeamHTML() {
+  ` <tr>
+  <td>${team.promotion}</td>
+  <td>${team.members}</td>
+  <td>c${team.name}
+    <td>
+      <a href="${team.url}">claudiu</a>
+      <td>x e</td>
+  </td>
+</tr>`;
+}
+
 function displayTeams(teams) {
   console.warn("display", teams);
   var teamsHTML = "";
   teams.forEach(function (team) {
-    teamsHTML += ` <tr>
-    <td>${team.promotion}</td>
-    <td>${team.members}</td>
-    <td>c${team.name}
-      <td>
-        <a href="${team.url}">claudiu</a>
-        <td>x e</td>
-    </td>
-  </tr>`;
+    teamsHTML += getTeamHTML(team);
   });
 
   document.querySelector("table tbody").innerHTML = teamsHTML;
