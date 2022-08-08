cf1a99571e2d14a082284eecd1b101b0155d8e8e
Claudiu Cimpean <cimpean1@gmail.com>
2022-08-08 20:41:12 +0300

    prepare submit team

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 function getTeamHTML(team) {
   return `
   <tr>
-  <td>${team.promotion}</td>
-  <td>${team.members}</td>
-  <td>c${team.name}
+    <td>${team.promotion}</td>
+    <td>${team.members}</td>
+    <td>${team.name}</td>
     <td>
-      <a href="${team.url}">openS</a>
-      <td>x e</td>
+      <a href="${team.url}">open</a>
+    </td>
+    <td>x e</td>
   </td>
-</tr>`;
+  </tr>`;
 }
 
 function displayTeams(teams) {
@@ -29,4 +30,24 @@ function loadTeams() {
       displayTeams(teams);
     });
 }
+function submitForm(e) {
+  e.preventDefault();
+  var promotion = document.querySelector("input[name=promotion]").value;
+  var members = document.querySelector("input[name=members]").value;
+  var name = document.querySelector("input[name=name]").value;
+  var url = document.querySelector("input[name=url]").value;
+  var team = {
+    promotion: promotion,
+    members: members,
+    name: name,
+    url: url,
+  };
+
+  console.warn("submit", JSON.stringify(team));
+}
+function initEvents() {
+  var form = document.getElementById("editForm");
+  form.addEventListener("submit", submitForm);
+}
 loadTeams();
+initEvents();
