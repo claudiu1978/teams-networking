57c072cd25cd892b9b80719e5d7d84c18f1f3449
Claudiu Cimpian <cimpean1@gmail.com>
2022-09-19 19:13:08 +0300

    improve footer input

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 let allTeams = [];
 let editId;
 
+function $(selector) {
+  return document.querySelector(selector);
+}
+
 function getTeamHTML(team) {
   return `
   <tr>
@@ -8,35 +12,29 @@ function getTeamHTML(team) {
     <td>${team.members}</td>
     <td>${team.name}</td>
     <td>
-      <a href="${team.url}">open</a>
+      <a href="${team.url}" target="_blank">open</a>
     </td>
     <td>
       <a href="#" data-id="${team.id}" class="delete-btn">✖</a>
-       <a href="#" data-id="${team.id}" class="edit-btn">&#9998;</a>
+      <a href="#" data-id="${team.id}" class="edit-btn">&#9998;</a>
     </td>
   </tr>`;
 }
 
 function displayTeams(teams) {
-  //  transform in html
   const teamsHTML = teams.map(getTeamHTML);
 
+  // afisare
   $("table tbody").innerHTML = teamsHTML.join("");
 }
 
 function loadTeams() {
   fetch("http://localhost:3000/teams-json")
     .then((r) => r.json())
-
-    .then(function (teams) {
-      console.warn("teams", teams);
+    .then((teams) => {
       allTeams = teams;
       displayTeams(teams);
     });
-}
-
-function $(selector) {
-  return document.querySelector(selector);
 }
 
 function createTeamRequest(team) {
@@ -56,7 +54,7 @@ function removeTeamRequest(id) {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ id: id }),
-  });
+  }).then((r) => r.json());
 }
 
 function getFormValues() {
@@ -64,6 +62,7 @@ function getFormValues() {
   const members = $("input[name=members]").value;
   const name = $("input[name=name]").value;
   const url = $("input[name=url]").value;
+
   const team = {
     promotion: promotion,
     members: members,
@@ -76,49 +75,49 @@ function getFormValues() {
 function setFormValues(team) {
   $("input[name=promotion]").value = team.promotion;
   $("input[name=members]").value = team.members;
-  $("input[name=name]").valueteam.name;
-  $("input[name=url]").value.url;
+  $("input[name=name]").value = team.name;
+  $("input[name=url]").value = team.url;
 }
 
 function submitForm(e) {
   e.preventDefault();
+
   const team = getFormValues();
+
   if (editId) {
     console.warn("pls edit", editId, team);
+  } else {
+    createTeamRequest(team)
+      .then((r) => r.json())
+      .then((status) => {
+        console.warn("status", status);
+        if (status.success) {
+          location.reload();
+        }
+      });
   }
-  createTeamRequest(team)
-    .then((r) => r.json())
-    .then((status) => {
-      console.warn("status", status);
-      if (status.success) {
-        location.reload();
-      }
-    });
 }
 
 function startEditTeam(id) {
   const team = allTeams.find((team) => team.id === id);
-  console.warn("edit", id);
   setFormValues(team);
   editId = id;
 }
 
 function initEvents() {
-  var form = document.getElementById("editForm");
+  const form = document.getElementById("editForm");
   form.addEventListener("submit", submitForm);
 
   form.querySelector("tbody").addEventListener("click", (e) => {
-    console.warn("click", e.target);
     if (e.target.matches("a.delete-btn")) {
-      const id = e.getAttribute("data-id");
-      console.warn("click pe link", id);
-      removeTeamRequest().then((status) => {
+      const id = e.target.getAttribute("data-id");
+      removeTeamRequest(id).then((status) => {
         if (status.success) {
           loadTeams();
         }
       });
     } else if (e.target.matches("a.edit-btn")) {
-      const id = e.getAttribute("data-id");
+      const id = e.target.getAttribute("data-id");
       startEditTeam(id);
     }
   });
